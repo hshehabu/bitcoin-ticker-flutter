1807408c9963303b95e10b364e9bea4d88a6cd14
hshehabu <hamzashehabu457@gmail.com>
2023-07-15 21:37:47 +0300

    async method to fetch currency data from coinapi

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const apiKey = 'C227589F-D055-4B0F-93D3-586AECD817BF';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +33,22 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future getData(String currency) async {
+
+    Map<String,String> cryptoCurrencies= {};
+    for(String crypto in cryptoList) {
+      Uri uri = Uri.parse(
+          'https://rest.coinapi.io/v1/exchangerate/$crypto/$currency?apikey=$apiKey');
+      http.Response response = await http.get(uri);
+
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        cryptoCurrencies[crypto] = decodedData['rate'].toStringAsFixed(1);
+      } else {
+        print(response.statusCode);
+      }
+    }
+    return cryptoCurrencies;
+  }
+}
